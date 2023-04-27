4ea4f9f4cb10bfb1f07c3dc56df9e7acc8ca0dfa
EnriqueChompre <enrique.chompre1@upr.edu>
2023-04-26 21:11:42 -0400

    Updated Main Unit
    Removed functions not in use and added voltage sensing code.

--- Main-unit/main.c
+++ Main-unit/main.c
@@ -19,11 +19,10 @@ char*ERROR_STRING[7] = {
 
 char LOT[30] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234";
 
-void checkExpDate();
 void raiseError(ERROR err);
 
-
-unsigned int currHour = 0;
+unsigned char ownerAddr[12];
+unsigned char subAddr[12];
 const unsigned int expDateHours = 17520;
 ERROR error = NONE;
 
@@ -31,84 +30,69 @@ int main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
 
-    ADCCTL0 |= ADCSHT_15 + ADCON;//Sample hold time 15, 1024 cycles
-    ADCCTL1 = ADCSHP;//Sample and hold pulse, sends a pulse when ready
-    ADCMCTL0 |= ADCINCH_5 + ADCSREF_7;//Voltage reference, ADCINCH_X= PIN 1.X, for temperature sensor
-    ADCIE |= ADCIE0;//sends interrupt when conversion is completed
-    ADCCTL0 |= ADCENC;//enable encoding for ADC
 
-    PM5CTL0 &= ~LOCKLPM5;//sends GPIO pins to low impedance mode
-
-    RTCMOD = (3600*32) - 1;//setting value for RTC to be an hour
-    RTCCTL = RTCSS__XT1CLK | RTCSR | RTCPS__1024 | RTCIE;//setting clock source, *, pre-devider 1024, enable interrupt
-
+//    PM5CTL0 &= ~LOCKLPM5;//sends GPIO pins to low impedance mode
+    P2OUT &= ~(BIT1 + BIT0);
     P2DIR |= BIT1+BIT0;//P2.1 = GREEN LED, P2.0= RED LED, P2.2= Voltage sensing pin
     P2OUT |= BIT1;
-    P2IES |= BIT2;
-    P2IFG &= ~BIT2;
-    P2IE |= BIT2;
-    //P1.2=VREF-, P1.0=VREF+, P1.5=Capture A5
-    P1SEL0 |= BIT6 + BIT7;//UART Pins P1.6=RX, P1.7=TX
-    UCA0CTLW0 |= UCSWRST;
-    UCA0CTLW0 |= UCSSEL_2;
-    UCA0BRW = 6; //baud rate
-    UCA0MCTLW = UCOS16 + (13<<4) + (0x22<<8);
-    UCA0CTLW0 &= ~UCSWRST;
-    UCA0IE |= UCRXIE;//enable interrupts for the receiver
+    P1IES |= BIT3;
+    P1REN |= BIT3;
+    P1IE |= BIT3;
 
-    __bis_SR_register(LPM4_bits + GIE);
-}
-
-#pragma vector=ADC_VECTOR
-__interrupt void ADC12ISR(void)
-{
-    voltage = ADCMEM0;//in memory, reference to 3.3V , offset from [600mV - 3.3V]
-    tempts[tempindex] = (voltage - 600) / 10.0;//offset convert to degrees, (Voltage-600mV)/(10mV/C)
-    tempindex = (++tempindex) % TEMPBUFSIZE;//circular index for temperature
+    P1IFG &= ~BIT3;
+//    P1SEL0 |= BIT6 + BIT7;//UART Pins P1.6=RX, P1.7=TX
+//    UCA0CTLW0 |= UCSWRST;
+//    UCA0CTLW0 |= UCSSEL_2;
+//    UCA0BR0 = 6; //baud rate
+//    UCA0BR1 = 0x00;
+//    UCA0MCTLW = UCOS16 + (13<<4) + (0x22<<8);
+//    UCA0CTLW0 &= ~UCSWRST;
+//    UCA0IE |= UCRXIE;//enable interrupts for the receiver
+    P3SEL = BIT4 + BIT5;
+    UCA0CTL1 |= UCSWRST;
+    UCA0CTL1 |= UCSSEL_2;
+    UCA0BR0 = 109;
+    UCA0BR1 = 0x00;
+    UCA0MCTL = UCBRS_2+UCBRF_0;
+    UCA0CTL1 &= ~UCSWRST;
+    UCA0IE |= UCTXIE + UCRXIE;
+    UCA0TXBUF = 'A';
+//    executeAT_Command("AT+IMME[]", "1", NULL);
+//    executeAT_Command("AT+ROLE[]", "1", NULL);
+//    ownerAddr=executeAT_Command("AT+ADDR?", NULL, NULL);
+//    subAddr= executeAT_Command("AT+DISC?", NULL, NULL);
+    __bis_SR_register(LPM3_bits | GIE);
 }
 
 #pragma vector=PORT2_VECTOR
 __interrupt void PORT2_ISR(void)
 {
-    if(P2IFG & BIT2){
-        P2IES ^= BIT2;//toggles interrupt edge
-        if (((P2IN>>2)& 1)==1){//verifies that power is present
-            P2OUT &= BIT0;
+    if(P2IFG & BIT3){
+        P1IES ^= BIT3;//toggles interrupt edge
+        if (((P1IN>>3)& 1)==1){//verifies that power is present
+            P2OUT &= ~BIT0;
             P2OUT |= BIT1;
             //toggles LEDs
         }
         else {
-            P2OUT |= BIT0;
-            P2OUT &= BIT1;
+            P2OUT &= ~BIT1;
+            P2OUT |= BIT1;
             //toggles LEDs
         }
-        P2IFG &= ~BIT2;//clears P2.2 flag
+        P1IFG &= ~BIT3;//clears P2.2 flag
     }
     else{
-        P2IFG &= BIT2;//clears all flags except P2.2
+        P1IFG &= BIT3;//clears all flags except P2.2
     }
 
 }
 
 
-#pragma vector=RTC_VECTOR
-__interrupt void RTC_ISR(void)
-{
-
-}
-
-void raiseError(ERROR err)
-{
-    error = err;
-    sprintf(BTTXBUF,"%s %s",LOT,ERROR_STRING[err]);
-    UCA0IE |= UCTXIE;
-
-}
 
 #pragma vector=USCI_A0_VECTOR
 __interrupt void USCI_A0_ISR(){
     switch(__even_in_range(UCA0IV,4)){//takes first 3 bits
-    case 0: break; //irrelavent
+    case 0: break; //Irrelevant
     case 2:
         if (rxcharindex < BTBUFSIZE && rxcharindex < strlen(BTRXBUF)){
             UCA0RXBUF = BTRXBUF[rxcharindex];//receiver buffer
