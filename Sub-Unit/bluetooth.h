2ca84b21b4b611e9aae2e1627113a68a4e363b1f
fabiomatos999 <78383665+fabiomatos999@users.noreply.github.com>
2023-04-29 09:18:34 -0400

    Added AT Commands to sub unit

--- Sub-Unit/bluetooth.h
+++ Sub-Unit/bluetooth.h
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 
 #define BTBUFSIZE 81
 char BTTXBUF[BTBUFSIZE];
@@ -9,62 +8,62 @@ unsigned int txcharindex = 0;
 unsigned int rxcharindex = 0;
 
 char *AT_COMMANDS[] = {
-        "AT\0",           // 0 Test Command
-        "AT+ADDR?\0",     // 1 Query module address
-        "AT+BAUD?\0",     // 2 Query baud rate
-        "AT+BAUD[]\0",    // 3 Set baud rate
-        "AT+CLEAR\0",     // 4 Clear Last Connected device address
-        "AT+CONNL\0",     // 5 Try connect to last succeeded device
-        "AT+CON[]\0",     // 6 Try connect an address
-        "AT+FILT?\0",     // 7 Query filter AT command parameter in AT+MODE value is 2
-        "AT+FILT[]\0",    // 8 Set filter AT command parameter in AT+MODE value is 2
-        "AT+HELP?\0",     // 9 System Help Information
-        "AT+IMME?\0",     // 10 Query Module work type
-        "AT+IMME[]\0",    // 11 Set Module work type
-        "AT+MODE?\0",     // 12 Query Module Work Mode
-        "AT+MODE[]\0",    // 13 Set Module Work Mode
-        "AT+NOTI\0",      // 14 Query Notify information
-        "AT+NOTI[]\0",    // 15 Set Notify information
-        "AT+NAME?\0",     // 16 Query Module name
-        "AT+NAME[]\0",    // 17 Set Module name
-        "AT+PARI?\0",     // 18 Query Parity bit
-        "AT+PARI[]\0",    // 19 Set Parity bit
-        "AT+PIO1?\0",     // 20 Query PIO1 output status (System LED)
-        "AT+PIO1[]\0",    // 21 Set PIO1 output status (System LED)
-        "AT+PIO[]?\0"     // 22 Query/Set PIO pins output high or low (Only this time, when module next power on, this value is not be used)
-        "AT+PIO[][]\0",   // 23 Query/Set PIO pins output high or low (Only this time, when module next power on, this value is not be used)
-        "AT+PASS?\0",     // 24 Query Pin Code
-        "AT+PIN[]\0",     // 25 Set Pin Code
-        "AT+POWE?\0",     // 26 Query Module Power
-        "AT+POWE[]\0",    // 27 Set Module Power
-        "AT+PWRM?\0",     // 28 Query Module sleep type
-        "AT+PWRM[]\0",    // 29 Set Module sleep type
-        "AT+RENEW\0",     // 30 Restore all setup value to factory setup
-        "AT+RESET\0",     // 31 Restart module
-        "AT+ROLE?\0",     // 32 Query Master and Slaver Role
-        "AT+ROLE[]\0",    // 33 Set Master and Slaver Role
-        "AT+RSSI?\0",     // 34 Query RSSI Value
-        "AT+RADD\0",      // 35 Query Last Connected Device Address
-        "AT+STOP?\0"      // 36 Query Stop bit
-        "AT+STOP[]\0",    // 37 Set Stop bit
-        "AT+START\0",     // 38 Work immediately
-        "AT+SLEEP\0",     // 39 Query Module into sleep mode
-        "AT+SAVE?\0",     // 40 Query Module save connected address parameter
-        "AT+SAVE[]\0",    // 41 Set Module save connected address parameter
-        "AT+TCON?\0",     // 42 Query module connect remote device timeout value
-        "AT+TCON[]\0",    // 43 Set module connect remote device timeout value
-        "AT+TYPE?\0",     // 44 Query Module Bond Mode
-        "AT+TYPE[]\0",    // 45 Set Module Bond Mode
-        "AT+VERR?\0",     // 46 Query Software Version
-        "AT+VERS?\0"      // 47 Query Software Version
+        "AT\r\n",           // 0 Test Command
+        "AT+ADDR?\r\n",     // 1 Query module address
+        "AT+BAUD?\r\n",     // 2 Query baud rate
+        "AT+BAUD[]\r\n",    // 3 Set baud rate
+        "AT+CLEAR\r\n",     // 4 Clear Last Connected device address
+        "AT+CONNL\r\n",     // 5 Try connect to last succeeded device
+        "AT+CON[]\r\n",     // 6 Try connect an address
+        "AT+FILT?\r\n",     // 7 Query filter AT command parameter in AT+MODE value is 2
+        "AT+FILT[]\r\n",    // 8 Set filter AT command parameter in AT+MODE value is 2
+        "AT+HELP?\r\n",     // 9 System Help Information
+        "AT+IMME?\r\n",     // 10 Query Module work type
+        "AT+IMME[]\r\n",    // 11 Set Module work type
+        "AT+MODE?\r\n",     // 12 Query Module Work Mode
+        "AT+MODE[]\r\n",    // 13 Set Module Work Mode
+        "AT+NOTI\r\n",      // 14 Query Notify information
+        "AT+NOTI[]\r\n",    // 15 Set Notify information
+        "AT+NAME?\r\n",     // 16 Query Module name
+        "AT+NAME[]\r\n",    // 17 Set Module name
+        "AT+PARI?\r\n",     // 18 Query Parity bit
+        "AT+PARI[]\r\n",    // 19 Set Parity bit
+        "AT+PIO1?\r\n",     // 20 Query PIO1 output status (System LED)
+        "AT+PIO1[]\r\n",    // 21 Set PIO1 output status (System LED)
+        "AT+PIO[]?\r\n"     // 22 Query/Set PIO pins output high or low (Only this time, when module next power on, this value is not be used)
+        "AT+PIO[][]\r\n",   // 23 Query/Set PIO pins output high or low (Only this time, when module next power on, this value is not be used)
+        "AT+PASS?\r\n",     // 24 Query Pin Code
+        "AT+PIN[]\r\n",     // 25 Set Pin Code
+        "AT+POWE?\r\n",     // 26 Query Module Power
+        "AT+POWE[]\r\n",    // 27 Set Module Power
+        "AT+PWRM?\r\n",     // 28 Query Module sleep type
+        "AT+PWRM[]\r\n",    // 29 Set Module sleep type
+        "AT+RENEW\r\n",     // 30 Restore all setup value to factory setup
+        "AT+RESET\r\n",     // 31 Restart module
+        "AT+ROLE?\r\n",     // 32 Query Master and Slaver Role
+        "AT+ROLE[]\r\n",    // 33 Set Master and Slaver Role
+        "AT+RSSI?\r\n",     // 34 Query RSSI Value
+        "AT+RADD\r\n",      // 35 Query Last Connected Device Address
+        "AT+STOP?\r\n"      // 36 Query Stop bit
+        "AT+STOP[]\r\n",    // 37 Set Stop bit
+        "AT+START\r\n",     // 38 Work immediately
+        "AT+SLEEP\r\n",     // 39 Query Module into sleep mode
+        "AT+SAVE?\r\n",     // 40 Query Module save connected address parameter
+        "AT+SAVE[]\r\n",    // 41 Set Module save connected address parameter
+        "AT+TCON?\r\n",     // 42 Query module connect remote device timeout value
+        "AT+TCON[]\r\n",    // 43 Set module connect remote device timeout value
+        "AT+TYPE?\r\n",     // 44 Query Module Bond Mode
+        "AT+TYPE[]\r\n",    // 45 Set Module Bond Mode
+        "AT+VERR?\r\n",     // 46 Query Software Version
+        "AT+VERS?\r\n"      // 47 Query Software Version
         };
 
-void resetBuffer(char *buf)
+void resetTXBuffer()
 {
     unsigned int i = 0;
     for (; i < BTBUFSIZE; i++)
     {
-        buf[i] = '\0';
+        BTTXBUF[i] = '\0';
     }
 }
 
@@ -83,13 +82,13 @@ int commandPosition(char *command)
 
 void printBufferNoArgs(char *buff, char *command)
 {
-    resetBuffer(buff);
+    resetTXBuffer();
     sprintf(buff, "%s", command);
 }
 
 void printBufferOneArg(char *buff, char *command, char *parm1)
 {
-    resetBuffer(buff);
+    resetTXBuffer();
     char commandCopy[BTBUFSIZE];
     strcpy(commandCopy, buff);
     unsigned int i = 0;
@@ -98,12 +97,12 @@ void printBufferOneArg(char *buff, char *command, char *parm1)
         if (commandCopy[i] == '[' || commandCopy[i] == ']')
             commandCopy[i] = '\0';
     }
-    sprintf(buff, "%s[%s]", command, parm1);
+    sprintf(buff, "%s%s\r\n", command, parm1);
 }
 
 void printBufferTwoArg(char *buff, char *command, char *parm1, char *parm2)
 {
-    resetBuffer(buff);
+    resetTXBuffer();
     char commandCopy[BTBUFSIZE];
     strcpy(commandCopy, buff);
     unsigned int i = 0;
@@ -112,7 +111,7 @@ void printBufferTwoArg(char *buff, char *command, char *parm1, char *parm2)
         if (commandCopy[i] == '[' || commandCopy[i] == ']')
             commandCopy[i] = '\0';
     }
-    sprintf(buff, "%s[%s][%s]", command, parm1, parm2);
+    sprintf(buff, "%s%s%s\r\n", command, parm1, parm2);
 }
 
 void executeAT_Command(char *command, char *parm1, char *parm2)
@@ -121,195 +120,195 @@ void executeAT_Command(char *command, char *parm1, char *parm2)
     {
     case 0:
         printBufferNoArgs(BTTXBUF, command);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 1:
         printBufferNoArgs(BTTXBUF, command);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 2:
         printBufferNoArgs(BTTXBUF, command);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 3:
         printBufferOneArg(BTTXBUF, command, parm1);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 4:
         printBufferNoArgs(BTTXBUF, command);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 5:
         printBufferNoArgs(BTTXBUF, command);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 6:
         printBufferNoArgs(BTTXBUF, command);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 7:
         printBufferNoArgs(BTTXBUF, command);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 8:
         printBufferOneArg(BTTXBUF, command, parm1);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 9:
         printBufferNoArgs(BTTXBUF, command);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 10:
         printBufferNoArgs(BTTXBUF, command);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 11:
         printBufferOneArg(BTTXBUF, command, parm1);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 12:
         printBufferNoArgs(BTTXBUF, command);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 13:
         printBufferOneArg(BTTXBUF, command, parm1);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 14:
         printBufferNoArgs(BTTXBUF, command);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 15:
         printBufferOneArg(BTTXBUF, command, parm1);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 16:
         printBufferNoArgs(BTTXBUF, command);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 17:
         printBufferOneArg(BTTXBUF, command, parm1);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 18:
         printBufferNoArgs(BTTXBUF, command);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 19:
         printBufferOneArg(BTTXBUF, command, parm1);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 20:
         printBufferNoArgs(BTTXBUF, command);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 21:
         printBufferOneArg(BTTXBUF, command, parm1);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 22:
         printBufferOneArg(BTTXBUF, command, parm1);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 23:
         printBufferTwoArg(BTTXBUF, command, parm1,parm2);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 24:
         printBufferNoArgs(BTTXBUF, command);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 25:
         printBufferOneArg(BTTXBUF, command, parm1);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 26:
         printBufferNoArgs(BTTXBUF, command);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 27:
         printBufferOneArg(BTTXBUF, command, parm1);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 28:
         printBufferNoArgs(BTTXBUF, command);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 29:
         printBufferOneArg(BTTXBUF, command, parm1);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 30:
         printBufferNoArgs(BTTXBUF, command);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 31:
         printBufferNoArgs(BTTXBUF, command);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 32:
         printBufferNoArgs(BTTXBUF, command);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 33:
         printBufferOneArg(BTTXBUF, command, parm1);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 34:
         printBufferNoArgs(BTTXBUF, command);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 35:
         printBufferNoArgs(BTTXBUF, command);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 36:
         printBufferNoArgs(BTTXBUF, command);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 37:
         printBufferOneArg(BTTXBUF, command, parm1);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 38:
         printBufferNoArgs(BTTXBUF, command);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 39:
         printBufferNoArgs(BTTXBUF, command);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 40:
         printBufferNoArgs(BTTXBUF, command);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 41:
         printBufferOneArg(BTTXBUF, command, parm1);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 42:
         printBufferNoArgs(BTTXBUF, command);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 43:
         printBufferOneArg(BTTXBUF, command, parm1);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 44:
         printBufferNoArgs(BTTXBUF, command);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 45:
         printBufferOneArg(BTTXBUF, command, parm1);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 46:
         printBufferNoArgs(BTTXBUF, command);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     case 47:
         printBufferNoArgs(BTTXBUF, command);
-        UCA0IE |= UCRXIE;
+        UCA0IFG |= BIT1;
         break;
     default:
         break;
--- Sub-Unit/main.c
+++ Sub-Unit/main.c
@@ -1,11 +1,68 @@
 #include <msp430.h> 
 #include <temperatureSensor.h>
+#include <string.h>
+#define BTBUFSIZE 81
 
-#define BUFFSIZE 40
+char BUFF[BTBUFSIZE];
+char BTTXBUF[BTBUFSIZE];
 
-char at[] = "AT\r\n";
+unsigned int txcharindex = 0;
+unsigned int startup = 0;
 
-char BUFF[BUFFSIZE];
+void resetTXBUFFER(){
+    unsigned int i = 0;
+    for(;i<BTBUFSIZE;i++){
+        BTTXBUF[i] = '\0';
+    }
+}
+
+void AT(){
+    char AT[] = "AT\r\n";
+    unsigned int i = 0;
+    for(;i<strlen(AT);i++){
+        while((UCA0STAT&UCBUSY));
+        UCA0TXBUF= AT[i];
+    }
+}
+
+void ATRESET(){
+    char AT[] = "AT+RESET\r\n";
+    unsigned int i = 0;
+    for(;i<strlen(AT);i++){
+        while((UCA0STAT&UCBUSY));
+        UCA0TXBUF= AT[i];
+    }
+}
+
+void ATNAME(){
+    char AT[] = "AT+NAMEOWO\r\n";
+    unsigned int i = 0;
+    for(;i<strlen(AT);i++){
+        while((UCA0STAT&UCBUSY));
+        UCA0TXBUF= AT[i];
+    }
+}
+
+void ATRENEW(){
+    char AT[] = "AT+RENEW\r\n";
+    unsigned int i = 0;
+    for(;i<strlen(AT);i++){
+        while((UCA0STAT&UCBUSY));
+        UCA0TXBUF= AT[i];
+    }
+}
+
+void ATCON(char ADDR[]){
+    char AT[19] = "AT+CON";
+    strcat(AT,ADDR);
+    strcat(AT,"\r\n");
+
+    unsigned int i = 0;
+    for(;i<strlen(AT);i++){
+        while((UCA0STAT&UCBUSY));
+        UCA0TXBUF= AT[i];
+    }
+}
 
 unsigned int i = 0;
 
@@ -36,22 +93,9 @@ ERROR error = NONE;
 
 int main(void)
 {
-    WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
 
-    P6SEL |= BIT7;
-    ADC12CTL0 |= ADC12SHT0_15 + ADC12ON;
-    ADC12CTL1 = ADC12SHP;
-    ADC12MCTL0 |= ADC12INCH_7 + ADC12SREF_7;
-    ADC12CTL0 |= ADC12ENC;
-//
-//
-    RTCCTL01 = RTCTEVIE + RTCSSEL_2 + RTCTEV_0; // Counter Mode, RTC1PS, 8-bit ovf
-                                              // overflow interrupt enable
-    RTCPS0CTL = RT0PSDIV_3;                   // ACLK, /8, start timer
-    RTCPS1CTL = RT1SSEL_2 + RT1PSDIV_4;       // out from RT0PS, /16, start timer
-//
-    P1DIR |= BIT4;
-    P1OUT &= ~BIT4;
+    resetTXBUFFER();
 
     P3SEL = BIT4 + BIT5;                             // P3.4,5 = USCI_A0 TXD/RXD
     UCA0CTL1 |= UCSWRST;                      // **Put state machine in reset**
@@ -61,11 +105,35 @@ int main(void)
     UCA0BR1 = 0;                              // 1MHz 115200
     UCA0MCTL |= UCBRS_2 + UCBRF_0;            // Modulation UCBRSx=1, UCBRFx=0
     UCA0CTL1 &= ~UCSWRST;                     // **Initialize USCI state machine**
-    UCA0IE |= UCTXIE;
+    ATRENEW();
+    __delay_cycles(100000);
+    ATRESET();
+    __delay_cycles(100000);
+    ATRENEW();
+    __delay_cycles(100000);
+    ATCON("845F042FF7F3");
+    __delay_cycles(100000);
+    startup = 1;
+    UCA0IE |= UCTXIE + UCRXIE;
+
+
+
+    P6SEL |= BIT7;
+    ADC12CTL0 |= ADC12SHT0_15 + ADC12ON;
+    ADC12CTL1 = ADC12SHP;
+    ADC12MCTL0 |= ADC12INCH_7 + ADC12SREF_7;
+    ADC12CTL0 |= ADC12ENC;
+
+    RTCCTL01 = RTCTEVIE + RTCSSEL_2 + RTCTEV_0; // Counter Mode, RTC1PS, 8-bit ovf
+                                              // overflow interrupt enable
+    RTCPS0CTL = RT0PSDIV_3;                   // ACLK, /8, start timer
+    RTCPS1CTL = RT1SSEL_2 + RT1PSDIV_4;       // out from RT0PS, /16, start timer
+
+    P1DIR |= BIT4;
+    P1OUT &= ~BIT4;
+
     __enable_interrupt();
 
-//    while(1){
-//    }
     __bis_SR_register(LPM0_bits);
 }
 
@@ -152,18 +220,20 @@ void raiseError(ERROR err)
 {
     error = err;
     P1OUT |= BIT4;
-    i = 0;
-    for(;i<30;i++){
-        BUFF[i] = LOT[i];
+    txcharindex = 0;
+    resetTXBUFFER();
+    for(;txcharindex<30;txcharindex++){
+        BTTXBUF[txcharindex] = LOT[txcharindex];
     }
-    BUFF[30] = ' ';
-    i = 31;
-    for(;i<38;i++){
-          BUFF[i] = ERROR_STRING[err][i-31];
+    BTTXBUF[30] = ' ';
+    txcharindex = 31;
+    for(;txcharindex<38;txcharindex++){
+        BTTXBUF[txcharindex] = ERROR_STRING[err][txcharindex-31];
       }
-    BUFF[37] = '\r';
-    BUFF[38] = '\n';
-    i=0;
+    BTTXBUF[37] = '\r';
+    BTTXBUF[38] = '\n';
+    BTTXBUF[39] = '\0';
+    txcharindex=0;
     UCA0IFG |= BIT1;
 }
 
@@ -174,20 +244,18 @@ __interrupt void USCI_A0_ISR(void)
   switch(__even_in_range(UCA0IV,4))
   {
   case 0:break;                             // Vector 0 - no interrupt
-  case 2:                                   // Vector 2 - RXIFG
+  case 2:
+      UCA0RXBUF = UCA0RXBUF;// Vector 2 - RXIFG
     break;
   case 4:
-      if (error == NONE || i >= BUFFSIZE) {
+      if ((error == NONE || txcharindex >= strlen(BTTXBUF) )&& startup == 1) {
           unsigned int x = 0;
-          for (; x < BUFFSIZE; x++)
-          {
-              BUFF[x] = '\0';
-          }
-          i = 0;
+          resetTXBUFFER();
+          txcharindex = 0;
       }
       else {
-          UCA0TXBUF = BUFF[i];
-          i++;
+          UCA0TXBUF = BTTXBUF[txcharindex];
+          txcharindex++;
       }
       break;
   default: break;
