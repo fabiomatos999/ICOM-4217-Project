eeb2238eef1a3cab371fd408ec767577d3b7d9f9
fabiomatos999 <fabio.matos@upr.edu>
2023-05-01 15:21:35 -0400

    Fixed log file generator

--- log-file.bash
+++ log-file.bash
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-#"(stty raw; cat > ~/power.log) < #1 &"
-#"(stty raw; cat > ~/error.log) < #2 &"
+touch ~/power.log
+touch ~/error.log
+
+(stty raw; cat > ~/power.log) < "$1" &
+(stty raw; cat > ~/error.log) < "$2" &
 
 while true
 do
     PREVNR="$(awk 'END{print NR}' ~/error.log)"
     sleep 20s
     DATE="$(date '+%Y %m %d %H:%M:%S')"
-    POWERSTATUS="$(tail -n 1 ~/power.log)"
+    POWERSTATUS="$(cat ~/power.log |{ read; echo "${REPLY#${REPLY%?}}";})"
     ERROR="$(tail -n 1 ~/error.log)"
     NR="$(awk 'END{print NR}' ~/error.log)"
     if [ "$PREVNR" == "$NR" ]; then
